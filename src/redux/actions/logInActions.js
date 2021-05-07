ab17e9e4cb8fa86ceee26ce8606f9ca6eccad9fd
carlos prieto <carlosprietobarron@gmail.com>
2021-05-07 05:53:54 -0500

    create login actions

---
+++ src/redux/actions/logInActions.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { BASEURL } from '../../API/apiData';
+import { LOG_IN_FAILURE, LOG_IN_REQUEST, LOG_IN_SUCCESS } from './logInTypes';
+
+export const logInRequest = () => ({
+  type: LOG_IN_REQUEST,
+});
+
+export const logInSuccess = response => ({
+  type: LOG_IN_SUCCESS,
+  payload: response,
+});
+export const logInFailure = error => ({
+  type: LOG_IN_FAILURE,
+  payload: error,
+});
+
+export const logIn = data => dispatch => {
+  dispatch(logInRequest());
+  return axios.post(`${BASEURL}login`, data)
+    .then(response => dispatch(logInSuccess(response.data)))
+    .catch(error => dispatch(logInFailure(error)));
+};
---
+++ src/redux/actions/loggedInActions.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { BASEURL } from '../../API/apiData';
+import { CHECK_LOGGED_IN_FAILURE, CHECK_LOGGED_IN_REQUEST, CHECK_LOGGED_IN_SUCCESS } from './loggedInTypes';
+
+
+export const checkLoggedInRequest = () => ({
+  type: CHECK_LOGGED_IN_REQUEST,
+});
+
+export const checkLoggedInSuccess = response => ({
+  type: CHECK_LOGGED_IN_SUCCESS,
+  payload: response,
+});
+export const checkLoggedInFailure = error => ({
+  type: CHECK_LOGGED_IN_FAILURE,
+  payload: error,
+});
+
+export const checkLoggedInStatus = () => dispatch => {
+  const token = localStorage.getItem('token')
+  dispatch(checkLoggedInRequest());
+  return axios.get(`${BASEURL}auto_login`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    }
+  })
+    .then(response => dispatch(checkLoggedInSuccess(response.data)))
+    .catch(error => dispatch(checkLoggedInFailure(error.message)));
+};
