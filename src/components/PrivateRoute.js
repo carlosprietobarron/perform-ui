15a0abd9cc7158e1d8d87f694cc1e0169472e5cc
carlos prieto <carlosprietobarron@gmail.com>
2021-05-07 05:51:53 -0500

    create modal form

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+import {Route, Redirect} from 'react-router-dom'
+
+const PrivateRoute = (isAuth, Component, ...rest) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PrivateRoute
