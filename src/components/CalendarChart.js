1bb24b851fc73ead204ef06bfdb8124009306e28
carlos prieto <carlosprietobarron@gmail.com>
2021-05-20 20:50:43 -0500

    Create calendar chart

--- src/components/CalendarChart.js
+++ src/components/CalendarChart.js
@@ -1,12 +1,32 @@
 import React, {useState, useEffect, useLayoutEffect} from "react";
 import Chart from "react-google-charts";
 
-const calendar = () => {
+const calendar = (data) => {
+  const [key, setkey] = useState();
+  const [state, setState] = useState({
+    status: "loading",
+    chartdata: [],
+  });
 
-  const [key, setkey] = useState(false);
-  //const [width, setWidth] = usState()
+  const convertDate =(meas)=>{
+    let arr = meas.split("-");
+    let ret = [];
+    arr.forEach(item => {
+      ret.push(parseInt(item));
+    });
+    return new Date(ret);
+  };
   const size = useWindowSize();
+  console.log("call calendar", data.measures);
 
+  const chartData = [['Indicator', 'Ocurrencies']];
+  for (let i = 0; i < data.measures.length; i += 1) {
+      console.log(convertDate(data.measures[i].day));
+      chartData.push([convertDate(data.measures[i].day), data.measures[i].measure])
+    }
+  
+  console.log(chartData);
+  
   useEffect(() => {
     setkey(prevKey => !prevKey);
     console.log("size", size);
@@ -25,7 +45,7 @@ const calendar = () => {
 
     return windowSize;
 }
-const windowSize = useWindowSize(900);
+  const windowSize = useWindowSize(900);
 
   return (
     <Chart
@@ -34,27 +54,9 @@ const windowSize = useWindowSize(900);
     height={"350px"}
     chartType="Calendar"
     loader={<div>Loading Chart</div>}
-    data={[
-      [
-        { type: "date", id: "Date" },
-        { type: "number", id: "Won/Loss" },
-      ],
-      [new Date(2012, 3, 13), 37032],
-      [new Date(2012, 3, 14), 38024],
-      [new Date(2012, 3, 15), 38024],
-      [new Date(2012, 3, 16), 38108],
-      [new Date(2012, 3, 17), 38229],
-      [new Date(2013, 1, 4), 38177],
-      [new Date(2013, 1, 5), 38705],
-      [new Date(2013, 1, 12), 38210],
-      [new Date(2013, 1, 13), 38029],
-      [new Date(2013, 1, 19), 38823],
-      [new Date(2013, 1, 23), 38345],
-      [new Date(2013, 1, 24), 38436],
-      [new Date(2013, 2, 10), 38447],
-    ]}
+    data={chartData}
     options={{
-      title: "Red Sox Attendance",
+      title: "Indicator's Measurements",
     }}
     rootProps={{ "data-testid": "1" }}
   />
@@ -64,12 +66,46 @@ const windowSize = useWindowSize(900);
 function CalendarChart({rawData, idx}) {
 
   console.log("raw data", rawData[idx]);
-
-  return (
+   
+ return (
     <div>
-      {calendar()}
+      {calendar(rawData[idx])}
     </div>
   );
 }
 
 export default CalendarChart;
+
+
+
+// [
+//   [
+//     { type: "date", id: "Date" },
+//     { type: "number", id: "Won/Loss" },
+//   ],
+//   [new Date(2012, 3, 13), 37032],
+//   [new Date(2012, 3, 14), 38024],
+//   [new Date(2012, 3, 15), 38024],
+//   [new Date(2012, 3, 16), 38108],
+//   [new Date(2012, 3, 17), 38229],
+//   [new Date(2013, 1, 4), 38177],
+//   [new Date(2013, 1, 5), 38705],
+//   [new Date(2013, 1, 12), 38210],
+//   [new Date(2013, 1, 13), 38029],
+//   [new Date(2013, 1, 19), 38823],
+//   [new Date(2013, 1, 23), 38345],
+//   [new Date(2013, 1, 24), 38436],
+//   [new Date(2013, 2, 10), 38447],
+// ]
+
+
+
+
+
+
+
+
+
+
+
+
